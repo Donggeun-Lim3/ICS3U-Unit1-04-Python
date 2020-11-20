baa1312a0f4f0b2cdc44849e50357d28fba0cd7c
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-20 20:16:45 +0000

    math

---
+++ math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by Donggeun Lim
+# Created on November 2020
+# This program is basic math
+
+def main():
+    # this function is math pogram
+    print("If a rectangle has the dimenstion:")
+    print("5cm x 3cm")
+    
+    print("Area is {}cm^2".format(5*3))
+    print("perimeter is {}cm".format(2*(5+3)))
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
